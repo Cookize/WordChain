07cd1bb2cd1a5b4d997659d15be0c65608a97a86
shihaobai <1798930569@qq.com>
2019-03-10 14:19:22 +0800

    warning

--- WordChain/WordChain/Solve.h
+++ WordChain/WordChain/Solve.h
@@ -18,7 +18,7 @@ public:
 		m_ModeHead = '&';
 		m_ModeTail = '&';
 	}
-	Solve(char mode,char c,bool is_head)
+	Solve(char mode, char c, bool is_head)
 	{
 		init();
 		temp_num = 0;
@@ -34,7 +34,7 @@ public:
 			m_ModeTail = c;
 		}
 	}
-	Solve(char mode,char mode_head,char mode_tail)
+	Solve(char mode, char mode_head, char mode_tail)
 	{
 		init();
 		temp_num = 0;
@@ -45,25 +45,25 @@ public:
 	void Solve1(WordList & wordlist, bool is_ring, vector<string> &output);
 	void solve_he(WordList& wordlist, vector<string> &outputc);
 
-	
+
 private:
-	int m_iSigned[26][26]; //问题一 记录 是字母是否使用
-	int m_ihead[26];
-	int m_ans[676][676]; //记录单词是否使用的变量
-	int len_ans[26][26];
-	int max_dfs[26];
-	int final_dfs[26];
-	int ahead[26];
-	int m_FinalLen;
-	int m_TemLen; 
-	int max_num;
-	int temp_num;
-	bool is_circle; // 判断是否有环 
-	int head;
-	char m_Mode;
-	char m_ModeHead;
-	char m_ModeTail;
-	bool m_ModeRing;
+	int m_iSigned[26][26] = {0}; //问题一 记录 是字母是否使用
+	int m_ihead[26] = {0};
+	int m_ans[676][676] = {0}; //记录单词是否使用的变量
+	int len_ans[26][26] = {0};
+	int max_dfs[26] = {0};
+	int final_dfs[26] = {0};
+	int ahead[26] = {0};
+	int m_FinalLen = 0;
+	int m_TemLen = 0; 
+	int max_num = 0;
+	int temp_num = 0;
+	bool is_circle = 0; // 判断是否有环 
+	int head = 0;
+	char m_Mode = NUM;
+	char m_ModeHead = '&';
+	char m_ModeTail = '&';
+	bool m_ModeRing = false;
     vector <string> m_FinalChain; //最终结果
 	vector <string> m_TempChain;  // 当前路径
 	void Dfs_solve1(WordList& wordlist, char c);
