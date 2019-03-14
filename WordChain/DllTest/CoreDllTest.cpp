3a8bcf3650d15ab0812174c7b3d2c63d9ce3fa2b
Cookize <38367063+Cookize@users.noreply.github.com>
2019-03-14 09:04:54 +0800

    Update CoreDllTest.cpp

--- WordChain/DllTest/CoreDllTest.cpp
+++ WordChain/DllTest/CoreDllTest.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include <Windows.h>
+#include <string>
 
-typedef (*genChainWord)
+typedef int(*genChainWord)(char* words[], int len, char*output[], char head, char tail, bool enable_loop);typedef int(*genChainChar)(char* words[], int len, char*output[], char head, char tail, bool enable_loop);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace DllTest
 {		
@@ -12,7 +14,15 @@ namespace DllTest
 	{
 	public:
 
+		HINSTANCE CoreDLL;
+		genChainWord gen_chain_word;
+		genChainChar gen_chain_char;
+
 		TEST_METHOD_INITIALIZE(init)
+		{
+			CoreDLL = LoadLibrary(wstring(L"CoreDll.dll").c_str());			if (CoreDLL == NULL) {				Assert::AreEqual(1, 0);			}			genChainWord gen_chain_word = (genChainWord)GetProcAddress(CoreDLL, "gen_chain_word");			genChainChar gen_chain_char = (genChainChar)GetProcAddress(CoreDLL, "gen_chain_char");			if (gen_chain_word == NULL || gen_chain_char == NULL) {				Assert::AreEqual(1, 0);			}
+		}
+		TEST_METHOD_CLEANUP(cleanUp)
 		{
 
 		}
